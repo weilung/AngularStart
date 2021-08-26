c5a823d5bf7a8d4974a0a2c53c17b2d7304f1a9c
weilung <2032ster@gmail.com>
2021-08-26 13:59:17 +0800

    3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'realworld-basic';
+  title = 'conduit';
+  subtitle = 'A place to share your knowledge';
 }
